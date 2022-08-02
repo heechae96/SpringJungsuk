5b9d98c3fb80bd9aba4a32913ff796f2dd2db146
heechae96 <shinheechae@naver.com>
2022-08-02 15:45:43 +0900

    원격 프로그램의 실행

---
+++ ch2/src/main/java/com/fastcampus/ch2/Hello.java
@@ -0,0 +1,31 @@
+package com.fastcampus.ch2;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+// 1. 원격 호출 가능한 프로그램으로 등록
+@Controller
+public class Hello {
+	
+	int iv = 10;			// 인스턴스 변수 
+	static int cv = 20;		// stiatc 변수
+
+	// 2. URL과 메서드를 연결
+	@RequestMapping("/hello")
+//	public void main() {	// 인스턴스 메서드 - iv, cv 둘 다 사용가능
+	private void main() {	// 인스턴스 메서드 - iv, cv 둘 다 사용가능
+		// 톰캣말고 콘솔에 출력을 한 것.
+		// 404뜬다고 호출실패한것이 아님!!
+//		System.out.println("Hello - static");
+		System.out.println("Hello - private");
+		
+		System.out.println(cv);		// ok
+		System.out.println(iv);		// ok
+	}
+	
+	public static void main2() {
+		// static메서드 - cv만 사용가능
+		System.out.println(cv);		// ok
+//		System.out.println(iv);		// error
+	}
+}
