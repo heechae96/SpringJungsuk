62c7e80bfdf1c9dfe63bd03d4d6b9cafc85b9001
heechae96 <shinheechae@naver.com>
2022-08-07 16:50:06 +0900

    관심사의 분리와 MVC패턴 - 원리(2)

--- ch2/src/main/java/com/fastcampus/ch2/YoilTellerMVC.java
+++ ch2/src/main/java/com/fastcampus/ch2/YoilTellerMVC.java
@@ -1,62 +1,46 @@
 package com.fastcampus.ch2;
 
+import java.util.Calendar;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import java.io.IOException;
-import java.util.Calendar;
-
-// 년월일을 입력하면 요일을 알려주는 프로그램 -> 스프링 패턴을 사용
 @Controller
 public class YoilTellerMVC {
+    @RequestMapping("/getYoilMVC") // http://localhost/ch2/getYoilMVC
+    public String main(int year, int month, int day, Model model) {
+ 
+        // 1. 유효성 검사
+    	if(!isValid(year, month, day)) 
+    		return "yoilError";  // 유효하지 않으면, /WEB-INF/views/yoilError.jsp로 이동
+    	
+        // 2. 처리
+    	char yoil = getYoil(year, month, day);
 
-	@RequestMapping("/getYoilMVC") // http://localhost/ch2/getYoilMVC?year=2021&month=10&day=1
-//	public String main(int year, int month, int day, Model model) throws IOException {
-//	public void main(int year, int month, int day, Model model) throws IOException {
-	public ModelAndView main(int year, int month, int day) throws IOException {
-		
-		ModelAndView mv = new ModelAndView();
-		
-		// 1. 유효성 검사
-//		if(!isValid(year, month, day)) {
-//			return "yoilError";		//	/WEB-INF/views/yoilError.jsp
-//		}
-		
-		// 2. 요일 계산
-		char yoil = getYoil(year, month, day);
-		
-		// 3. 계산한 결과를 model에 저장
-//		model.addAttribute("year", year);
-//		model.addAttribute("month", month);
-//		model.addAttribute("day", day);
-//		model.addAttribute("yoil", yoil);
+        // 3. Model에 작업 결과 저장
+        model.addAttribute("year", year);
+        model.addAttribute("month", month);
+        model.addAttribute("day", day);
+        model.addAttribute("yoil", yoil);
+        
+        // 4. 작업 결과를 보여줄 View의 이름을 반환
+        return "yoil"; // /WEB-INF/views/yoil.jsp
+    }
+    
+    private char getYoil(int year, int month, int day) {
+        Calendar cal = Calendar.getInstance();
+        cal.set(year, month - 1, day);
 
-		mv.addObject("year", year);
-		mv.addObject("month", month);
-		mv.addObject("day", day);
-		mv.addObject("yoil", yoil);
-		
-//		return "yoil";	//	/WEB-INF/views/yoil.jsp
-		
-		// 4. 결과를 보여줄 view를 지정
-		mv.setViewName("yoil");
-		
-		return mv; 
-
-	}
-
-	private boolean isValid(int year, int month, int day) {
-//		return false;	// 항상 yoilError페이지
-		return true;	
-	}
-
-	private char getYoil(int year, int month, int day) {
-		Calendar cal = Calendar.getInstance();
-		cal.set(year, month - 1, day);
-
-		int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK);	// 1: 일요일, 2: 월요일, ...
-		return " 일월화수목금토".charAt(dayOfWeek);
-	}
+        int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK);
+        return " 일월화수목금토".charAt(dayOfWeek);
+    }
+    
+    private boolean isValid(int year, int month, int day) {    
+    	if(year==-1 || month==-1 || day==-1) 
+    		return false;
+    	
+    	return (1<=month && month<=12) && (1<=day && day<=31); // 간단히 체크 
+    }
 }
