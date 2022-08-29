8e9b807400d0905fb5618a8468857d974c512392
heechae96 <shinheechae@naver.com>
2022-08-30 00:23:20 +0900

    세션(Session)-실습

--- ch2/src/main/java/com/fastcampus/ch2/LonginController.java
+++ ch2/src/main/java/com/fastcampus/ch2/LonginController.java
@@ -3,7 +3,9 @@ package com.fastcampus.ch2;
 import java.net.URLEncoder;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,14 +20,23 @@ public class LonginController {
 	public String loginForm() {
 		return "loginForm";
 	}
+	
+	@GetMapping("/logout")
+	public String logout(HttpSession session) {	// session을 HttpServletRequest말고 HttpSession을 통해 직접 받을수 있다(스프링이 자동으로 넣어주는것)
+		// 1. 세션을 종료
+		session.invalidate();
+		// 2. 홈으로 이동
+		return "redirect:/";
+	}
 
 	@PostMapping("/login")
-	public String login(String id, String pwd, boolean rememberId, HttpServletResponse response) throws Exception {
+	public String login(String id, String pwd, boolean rememberId, 
+							HttpServletRequest request, HttpServletResponse response) throws Exception {
 
 		// 쿼리스트링 값들이 어떤식으로 들어오는가 확인
 		// rememberId(type="checkbox")를 String으로 하면 on이 찍힘. 디폴트 value값이 on.
 		// boolean으로 하면 체크의 경우. true / 체크가 안 된 경우. false
-		System.out.println("id: " + id + ", pwd: " + pwd + ", rememberId: " + rememberId);
+//		System.out.println("id: " + id + ", pwd: " + pwd + ", rememberId: " + rememberId);
 
 		// 1. id와 pwd를 확인
 		if (!loginCheck(id, pwd)) {
@@ -36,6 +47,11 @@ public class LonginController {
 		}
 
 		// 2-2. i와 pwd가 일치하면,
+		//	-> 세션 객체를 얻어오기
+		HttpSession session = request.getSession();
+		//	->세션 객체에 id를 저장
+		session.setAttribute("id", id);
+		
 		if (rememberId) { // 아이디 기억 체크박스 선택
 			// 1. 쿠키를 생성
 			Cookie cookie = new Cookie("id", id);
