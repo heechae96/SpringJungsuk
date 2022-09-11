0c4c231fa98e6212c7440829d9674239811acc4e
heechae96 <shinheechae@naver.com>
2022-09-11 15:40:59 +0900

    Spring DI 활용하기 - 이론

--- ch3/src/main/java/com/fastcampus/ch3/ApplicationContextTest.java
+++ ch3/src/main/java/com/fastcampus/ch3/ApplicationContextTest.java
@@ -1,14 +1,16 @@
 package com.fastcampus.ch3;
 
-import org.springframework.beans.factory.annotation.*;
-import org.springframework.context.*;
-import org.springframework.context.annotation.*;
-import org.springframework.context.annotation.Scope;
-import org.springframework.context.support.*;
-import org.springframework.stereotype.*;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Properties;
 
-import javax.inject.*;
-import java.util.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.support.GenericXmlApplicationContext;
+import org.springframework.stereotype.Component;
 
 @Component
 @Scope("prototype")
@@ -56,9 +58,38 @@ class Car {
 	}
 }
 
+@Component
+@PropertySource("setting.properties")
+class SysInfo {
+	@Value("#{systemProperties['user.timezone']}")
+	String timeZone;
+	@Value("#{systemEnvironment['TMPDIR']}")
+	String tempDir;
+	@Value("${autoSaveDir}")
+	String autoSaveDir;
+	@Value("${autoSaveInterval}")
+	int autoSaveInterval;
+	@Value("${autoSave}")
+	boolean autoSave;
+
+	@Override
+	public String toString() {
+		return "SysInfo [timeZone=" + timeZone + ", tempDir=" + tempDir + ", autoSaveDir=" + autoSaveDir
+				+ ", autoSaveInterval=" + autoSaveInterval + ", autoSave=" + autoSave + "]";
+	}
+
+}
+
 public class ApplicationContextTest {
 	public static void main(String[] args) {
 		ApplicationContext ac = new GenericXmlApplicationContext("config.xml");
+
+		System.out.println("ac.getBean(SysInfo.class) = " + ac.getBean(SysInfo.class));
+		Map<String, String> map = System.getenv();
+		System.out.println("map = " + map);
+		Properties properties = System.getProperties();
+		System.out.println("properties = " + properties);
+
 //      Car car = ac.getBean("car", Car.class); // 타입을 지정하면 형변환 안해도됨. 아래의 문장과 동일
 		Car car = (Car) ac.getBean("car"); // 이름으로 빈 검색
 //        Car car2 = (Car) ac.getBean(Car.class);   // 타입으로 빈 검색
